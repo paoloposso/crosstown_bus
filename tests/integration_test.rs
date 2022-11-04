a3c258e0ef755d8a7f9669c0c8274bee82c0431a
Paolo <pvictorsys@gmail.com>
2022-11-04 15:10:44 -0300

    fix unit tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -20,31 +20,29 @@ impl MessageHandler<String> for MyCustomHandler {
     }
 }
 
-pub struct MyCustomHandler2;
+pub struct UserCreatedEventHandler;
 
-impl MessageHandler<UserCreatedEventMessage> for MyCustomHandler2 {
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedEventHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), String> {
-        println!("Message received on handler 2: {:?}", message);
+        println!("Message received on User Created Handler: {:?}", message);
         Ok(())
     }
 }
 
 #[test]
 fn create_subscription() -> Result<(), Box<dyn Error>> {
-    let subscriber = QueueBus::<String>::new("amqp://guest:guest@localhost:5672".to_owned())?;
-    let subscriber2 = QueueBus::<UserCreatedEventMessage>::new("amqp://guest:guest@localhost:5672".to_owned())?;
+    let subscriber = QueueBus::<UserCreatedEventMessage>::new("amqp://guest:guest@localhost:5672".to_owned())?;
     let mut publ = QueueBus::<String>::new("amqp://guest:guest@localhost:5672".to_owned())?;
 
-    let _ = futures::executor::block_on(subscriber
-        .add_subscription::<String>("queue1".to_owned(),  Arc::new(MyCustomHandler))?
-        .add_subscription::<String>("queue2".to_owned(),  Arc::new(MyCustomHandler))?
-        .subscribe_registered_events());
+    let _ = futures::executor::block_on(
+        subscriber
+            .add_subscription::<UserCreatedEventMessage>("queue3".to_owned(),  Arc::new(UserCreatedEventHandler))?
+            .add_subscription::<UserCreatedEventMessage>("queue4".to_owned(),  Arc::new(UserCreatedEventHandler))?
+            .subscribe_registered_events()
+    );
     
-        _ = publ.publish_event("queue1".to_owned(), "123456".to_owned());
-    _ = publ.publish_event("queue1".to_owned(), "123456".to_owned());
-    _ = publ.publish_event("queue1".to_owned(), "123456".to_owned());
-    _ = publ.publish_event("queue2".to_owned(), "queue2 aaaaa".to_owned());
-    
+    _ = publ.publish_event("queue3".to_owned(), UserCreatedEventMessage { user_id: "11111".to_owned(), user_name: "paolo".to_owned(), timestamp: 3943043274 });
+    _ = publ.publish_event("queue4".to_owned(), UserCreatedEventMessage { user_id: "22222".to_owned(), user_name: "paolo".to_owned(), timestamp: 3943043274 });
     
     let _ = thread::sleep(Duration::from_secs(4));
     let _err = publ.close_connection();
