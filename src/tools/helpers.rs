19c1e56eabc9afded67cdd7ff4a06cab347a322d
Paolo <pvictorsys@gmail.com>
2022-11-05 17:21:19 -0300

    hide unused

--- src/tools/helpers.rs
+++ src/tools/helpers.rs
@@ -1,4 +1,4 @@
-pub fn get_event_name<T>() -> String {
+pub(crate) fn get_event_name<T>() -> String {
     let full_event_name = std::any::type_name::<T>().to_string();
     let event_array = full_event_name.split("::").collect::<Vec<&str>>();
     event_array.last().unwrap().to_string()
