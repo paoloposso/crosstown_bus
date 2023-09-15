370c4f8c46346565d24170f03d46ecf5c90f4caf
Paolo Posso <pvictorsys@gmail.com>
2023-09-15 09:46:29 -0300

    Run tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,10 @@ mod broadcast_publisher;
 mod broadcast_subscriber;
 mod common;
 
-use std::cell::Cell;
-use std::collections::HashMap;
-
 use std::{error::Error, cell::RefCell};
 
 use amiquip::Connection;
-use broadcast_subscriber::{SubscriptionManager, BroadcastSubscriber};
-use queue_bus::{QueuePublisher, QueueListener};
+use queue_bus::{Sender, Receiver};
 pub use common::QueueProperties;
 pub use message_handler::MessageHandler;
 pub use message_handler::HandleError;
@@ -24,29 +20,29 @@ pub use broadcast_publisher::BroadcastPublisher;
 pub struct CrosstownBus();
 
 impl CrosstownBus {
-    pub fn new_queue_listener(url: String) -> Result<QueueListener, Box<dyn Error>> {
-        Ok(QueueListener {
+    pub fn new_receiver(url: String) -> Result<Receiver, Box<dyn Error>> {
+        Ok(Receiver {
             cnn: RefCell::new(Connection::insecure_open(&url)?)
         })
     }
     
-    pub fn new_queue_publisher(url: String) -> Result<QueuePublisher, Box<dyn Error>> {
-        Ok(QueuePublisher {
+    pub fn new_sender(url: String) -> Result<Sender, Box<dyn Error>> {
+        Ok(Sender {
             cnn: RefCell::new(Connection::insecure_open(&url)?)
         })
     }
 
-    pub fn new_broadcast_publisher(url: String) -> Result<BroadcastPublisher, Box<dyn Error>> {
-        Ok(BroadcastPublisher(
-            RefCell::new(Connection::insecure_open(&url)?)
-        ))
-    }
+    // pub fn new_broadcast_publisher(url: String) -> Result<BroadcastPublisher, Box<dyn Error>> {
+    //     Ok(BroadcastPublisher(
+    //         RefCell::new(Connection::insecure_open(&url)?)
+    //     ))
+    // }
 
-    pub fn new_broadcast_subscriber<T>(url: String) -> Result<BroadcastSubscriber::<T>, Box<dyn Error>> 
-        where T: borsh::BorshSerialize, T: borsh::BorshDeserialize {
-        Ok(BroadcastSubscriber {
-            cnn: Cell::new(Connection::insecure_open(&url)?),
-            subs_manager: SubscriptionManager::<T> { handlers_map: HashMap::default() }
-        })
-    }
+    // pub fn new_broadcast_subscriber<T>(url: String) -> Result<BroadcastSubscriber::<T>, Box<dyn Error>> 
+    //     where T: borsh::BorshSerialize, T: borsh::BorshDeserialize {
+    //     Ok(BroadcastSubscriber {
+    //         cnn: Cell::new(Connection::insecure_open(&url)?),
+    //         subs_manager: SubscriptionManager::<T> { handlers_map: HashMap::default() }
+    //     })
+    // }
 }
--- tests/send_receive_integration_test.rs
+++ tests/send_receive_integration_test.rs
@@ -1,24 +1,45 @@
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{MessageHandler, CrosstownBus, HandleError, QueueProperties};
+use crosstown_bus::{
+    CrosstownBus, HandleError, MessageHandler, QueueProperties,
+};
 
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
     pub user_id: String,
-    pub user_name: String
+    pub user_name: String,
 }
 
-pub struct UserCreatedEventHandler;
+pub struct UserCreatedEventHandler {
+    received_messages: Arc<Mutex<Vec<UserCreatedEventMessage>>>,
+}
+
+impl UserCreatedEventHandler {
+    pub fn new(received_messages: Arc<Mutex<Vec<UserCreatedEventMessage>>>) -> Self {
+        Self { received_messages }
+    }
+
+    pub fn get_received_messages(&self) -> Vec<UserCreatedEventMessage> {
+        self.received_messages.lock().unwrap().clone()
+    }
+}
 
 impl MessageHandler<UserCreatedEventMessage> for UserCreatedEventHandler {
     fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        self.received_messages.lock().unwrap().push(*message.clone());
+
         if message.user_id == "100".to_owned() {
             return Err(HandleError::new("ID 100 rejected".to_owned(), false));
         }
         println!("Message received on User Created Handler: {:?}", message);
+
         Ok(())
     }
+
     fn get_handler_action(&self) -> String {
         todo!()
     }
@@ -26,25 +47,55 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedEventHandler {
 
 #[test]
 fn send_receive() -> Result<(), Box<dyn Error>> {
-    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned())?;
+    let received_messages = Arc::new(Mutex::new(Vec::new()));
+    let listener = CrosstownBus::new_receiver("amqp://guest:guest@localhost:5672".to_owned())?;
 
-    _ = listener.listen("user_created".to_owned(), UserCreatedEventHandler, 
-        QueueProperties { auto_delete: false, durable: false, use_dead_letter: true });
+    listener.receive(
+        "create_user".to_owned(),
+        UserCreatedEventHandler::new(received_messages.clone()),
+        QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    )?;
 
-    let mut publisher = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned())?;
-    _ = publisher.publish_event("user_created".to_owned(), 
+    let mut publisher =
+        CrosstownBus::new_sender("amqp://guest:guest@localhost:5672".to_owned())?;
+
+    publisher.send(
+        "create_user".to_owned(),
         UserCreatedEventMessage {
             user_id: "asdf".to_owned(),
-            user_name: "Geddy Lee".to_owned()
-        });
+            user_name: "Geddy Lee".to_owned(),
+        },
+    )?;
 
-    _ = publisher.publish_event("user_created".to_owned(), 
+    publisher.send(
+        "create_user".to_owned(),
         UserCreatedEventMessage {
             user_id: "1234".to_owned(),
-            user_name: "Steven Tyler".to_owned()
-        });
+            user_name: "Steven Tyler".to_owned(),
+        },
+    )?;
 
-    _ = publisher.close_connection();
+    publisher.send(
+        "create_user".to_owned(),
+        UserCreatedEventMessage {
+            user_id: "100".to_owned(),
+            user_name: "Roger Waters".to_owned(),
+        },
+    )?;
 
+    // Sleep for a moment to allow messages to be processed
+    thread::sleep(Duration::from_secs(1));
+
+    let received_messages = received_messages.lock().unwrap();
+    assert_eq!(received_messages.len(), 3);
+
+    // Optionally, you can further assert the content of received messages
+    assert!(received_messages[0].user_id == "asdf" || received_messages[0].user_id == "1234" || received_messages[0].user_id == "100");
+
+    publisher.close_connection()?;
     Ok(())
 }
